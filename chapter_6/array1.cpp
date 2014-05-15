76048d5dc4b06e13e611496a27575ab7dda5ace3
Peixiang xu <peixiangxu@gmail.com>
2014-05-15 08:39:11 +0800

    add array stl demo

---
+++ chapter_6/array1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+int main()
+{
+    int col[] = {5, 6, 2, 4, 1, 3};
+
+    // col + 6
+    transform(col, col + 6
+            , col
+            , col
+            , multiplies<int>());
+
+    sort(col + 1, col + 6);
+
+    copy(col, col + 6, ostream_iterator<int>(cout, "  "));
+    cout << endl;
+}
