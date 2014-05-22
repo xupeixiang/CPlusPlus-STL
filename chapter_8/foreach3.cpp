2fed3f268d6d0d7e5a9f72c7cd46dd3ec7d12ae1
Peixiang xu <peixiangxu@gmail.com>
2014-05-22 08:40:27 +0800

    add foreach demo

---
+++ chapter_8/foreach3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "genera.h"
+using namespace std;
+
+class MeanValue
+{
+    private:
+        long num;
+        long sum;
+    public:
+        MeanValue(): num(0), sum(0) {}
+
+        void operator() (int elem)
+        {
+            num++;
+            sum += elem;
+        }
+
+        double value()
+        {
+            return static_cast<double>(sum) / static_cast<double>(num);
+        }
+};
+
+int main()
+{
+    vector<int> coll;
+    generate_n(back_inserter(coll),
+              8,
+              IntSequence(1));
+    MeanValue mv = for_each(coll.begin(), coll.end(), MeanValue());
+    cout << "mean value: " << mv.value() << endl;
+}
