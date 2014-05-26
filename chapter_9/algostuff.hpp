9ac7fc6f58a1443bac58ba764d2fb2b4fc09261b
Peixiang xu <peixiangxu@gmail.com>
2014-05-26 21:42:21 +0800

    add helper functions

---
+++ chapter_9/algostuff.hpp
@@ -0,0 +1,35 @@
+#ifndef ALFOSTUFF_HPP
+#define ALFOSTUFF_HPP
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+template <class T>
+inline void PRINT_ELEMENTS(const T& coll, const char* optstr = "")
+{
+    typename T::const_iterator pos;
+    std::cout << optstr;
+    for (pos = coll.begin(); pos != coll.end(); ++pos)
+    {
+        std::cout << *pos << "  ";
+    }
+    std::cout << std::endl;
+}
+
+inline void INSERT_ELEMENTS(T& coll, int first, int last)
+{
+    for (int i = first; i <= last; ++i)
+    {
+        // general insert
+        coll.insert(coll.end(), i);
+    }
+}
+
+#endif
